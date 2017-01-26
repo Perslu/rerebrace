77b5fdd0699bf6b32c91b2e952ac96bfaaf6bee1
LAPTOP-CB5AGFP1\Magda <magdalena.goorna@gmail.com>
2017-01-26 15:01:12 +0100

    test for UIButton

--- src/mobile/UI/button/UIButton/UIButton.test-data.js
+++ src/mobile/UI/button/UIButton/UIButton.test-data.js
@@ -1,16 +1,24 @@
-import {assert} from 'chai'
-import {shallow} from 'enzyme'
+import React from 'react'
+import {assert, expect} from 'chai'
+import {shallow, render} from 'enzyme'
 import UIButton from './index'
 
 describe('UIButton Component', () => {
   it('should render', () => {
-
+    const wrapper = render(<UIButton />);
+    //assert.equal(wrapper.find('button').length, 1);
+    expect(wrapper.find('button').length).to.equal(1);
     //assert('foo' === 'bar', 'foo is not bar');
     //assert(Array.isArray([]), 'empty arrays are arrays');
   });
-  it('should render button', () => {
-    const wrapper = shallow(<UIButton />);
-    assert.equal(wrapper.find('button').length, 1);
-    assert.equal(wrapper.find('button.default').length, 1, 'class default not exist');
+  it('by default should add default class', () => {
+    const wrapper = render(<UIButton />);
+    expect(wrapper.find('button').attr('class').indexOf('default')).to.not.equal(-1);
+    expect(wrapper.find('button').attr('class').indexOf('primary')).to.equal(-1);
+  });
+  it('should add primary class when primary props given', () => {
+    const wrapper = render(<UIButton primary/>);
+    expect(wrapper.find('button').attr('class')).to.include('primary');
+    expect(wrapper.find('button').attr('class')).to.not.include('default');
   });
 });
